c726de8c2305928db81b55fb85405ee977cab73a
Daniel Oliveira Barbosa <daniel.obarbosa@quintoandar.com.br>
2025-09-20 11:46:36 -0300

    feat: add user model

--- better_call/backend/models/__init__.py
+++ better_call/backend/models/__init__.py
@@ -1,4 +1,5 @@
 from .requests import CallRequest
 from .responses import CallResponse, HealthResponse
+from .user import User, UserPublic
 
-__all__ = ["CallRequest", "CallResponse", "HealthResponse"]
+__all__ = ["CallRequest", "CallResponse", "HealthResponse", "User", "UserPublic"]
